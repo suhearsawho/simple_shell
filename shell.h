dd5a20b61ff810e70dce68f210aac464ecb9b931
susansu <susansu.software@gmail.com>
2018-11-11 00:09:20 +0000

    function prototypes for shell project

---
+++ shell.h
@@ -0,0 +1,18 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+typedef struct cmds {
+	struct cmds *next;
+	int (*run_cmd)(char *);
+} cmd_t;
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <string.h>
+
+#endif
