487b22ad829982dc7ce0065e6bd20b04bf4c03bc
Martin Smith <538@holbertonschool.com>
2018-11-12 07:38:02 +0000

    rename simple _getline

---
+++ s_getline.c
@@ -0,0 +1,44 @@
+#include "shell.h"
+#include <stdio.h>
+#include <unistd.h>
+
+
+static size_t BUFSIZE = 120;
+
+ssize_t _getline(char **line, size_t *n, FILE *stream)
+{
+	ssize_t rd;
+	size_t i = BUFSIZE;
+	char *buf;
+	
+	(void) stream;
+
+	if (*n > BUFSIZE)
+		BUFSIZE = i = *n;
+	if (!*line)
+		buf = malloc(sizeof(char) * BUFSIZE);
+	else
+	{
+/*		buf = *line;
+		do {
+			*buf++ = 0;
+		} while (--i); 
+*/
+		buf = *line;
+	}
+	while ((rd = read(0, buf, BUFSIZE)) > 0)
+	{
+		strtok(buf, "\r\n");
+		if (rd == 1 )
+			return (0);
+		*n = BUFSIZE;
+		if ((size_t)rd == BUFSIZE)
+			*n = BUFSIZE *= 2;
+		*line = buf;
+		if ((size_t)rd < BUFSIZE && rd > 1)
+			return rd;
+		else if (rd == 1)
+			return 1;
+	}
+	return -1;
+}
