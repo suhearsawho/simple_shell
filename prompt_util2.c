ed88e335c8cb876a3d69612c138ad563993bbcd7
susansu <susansu.software@gmail.com>
2018-11-16 22:05:24 +0000

    cleaned up printing error codes \n

--- prompt_util2.c
+++ prompt_util2.c
@@ -22,19 +22,17 @@ void p_commanderr(char *command, char *filename)
 	char *error_message;
 	char *error = ": command not found\n";
 	size_t num_char, i;
-
 	(void)filename;
+
 	num_char = _strlen(error) + _strlen(command);
 	error_message = malloc(sizeof(char) * (++num_char));
 
 	i = 0;
 	while (*command != '\0')
-	{
 		error_message[i++] = *command++;
-	}
 	while (*error != '\0')
 		error_message[i++] = *error++;
 	error_message[i] = '\0';
-	write(STDOUT_FILENO, error_message, num_char);
+	write(STDERR_FILENO, error_message, num_char);
 	free(error_message);
 }
